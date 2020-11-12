241b59b02c6ded12d976dd21f77a8c13c8c94c5f
Agustin Pane <agustin.pane@gmail.com>
2020-11-12 15:37:21 -0300

    Remove unused pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { HomeComponent } from './components/home/home.component';
 import { SimpleComponent } from './components/simple/simple.component';
-import { NgmodelComponent } from './components/ngmodel/ngmodel.component';
 import { FormcontrolComponent } from './components/formcontrol/formcontrol.component';
-import { NoanimationsComponent } from './components/noanimations/noanimations.component';
-import { ClosedComponent } from './components/closed/closed.component';
 import { CollapsibleComponent } from './components/collapsible/collapsible.component';
-import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
-import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,25 +15,19 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { RadialColorPickerModule } from "./components/lib/radial-color-picker.module";
+import { RadialColorPickerModule } from './components/lib/radial-color-picker.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     SimpleComponent,
-    NgmodelComponent,
     FormcontrolComponent,
-    NoanimationsComponent,
-    ClosedComponent,
     CollapsibleComponent,
-    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule,
     RadialColorPickerModule,
     MatSidenavModule,
     MatIconModule,
